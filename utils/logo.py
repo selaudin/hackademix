39a19c909fb5773ddde6203c6f21fff00045e4cb
Selaudin Agolli <selaudin.agolli@mdpi.com>
2024-11-12 15:59:09 +0000

    Update logo url path

--- utils/logo.py
+++ utils/logo.py
@@ -5,12 +5,12 @@ import base64
 from io import BytesIO
 
 def add_logo():
-    with open("assets/HackademixLogo.png", "rb") as f:
+    with open("assets/hackademix_logo_trans.png", "rb") as f:
         image_data = f.read()
     # Resize the image
     image = Image.open(BytesIO(image_data))
     width, height = image.size
-    image = image.resize((285, 60), Image.LANCZOS) 
+    image = image.resize((275, 50), Image.LANCZOS) 
     # Convert the image to a base64-encoded string
     buffered = BytesIO()
     image.save(buffered, format="PNG")
